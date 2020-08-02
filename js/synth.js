9d1e7ceacdb175b113fc975ac0292ec7e08e9cce
Shawn O. Eary <seary@uark.edu>
2020-08-02 16:10:11 -0500

    Got a single bomb dropping sound but have to hit refresh after each dropping.

--- js/synth.js
+++ js/synth.js
@@ -1,46 +1,66 @@
+// I can't believe I'm actually declaring a
+// global variable (gasp).  Shame on me.
+// More globals are sure to come...
+//
+// Maybe I should throw in a few GOTO statements
+// for good measure (smirk)
+// https://www.w3schools.com/js/js_arrays.asp
+var bombs = [];
+
 // https://stackoverflow.com/questions/879152/how-do-i-make-javascript-beep
 function start() {
-    // Create up to ten bombs
+    // Create up one bomb
     // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_random
-    var numBombs = Math.floor(Math.random() * 10.0) + 1;
+    // var numBombs = Math.floor(Math.random() * 10.0) + 1;
+    var numBombs = 1;
 
-    // https://www.w3schools.com/js/js_arrays.asp
-    var bombs = [];
     for (var i = 0; i < numBombs; i++) {
         // Create a bomb between 400 and 800 ("Martian Feet") - Yeah Whatever..
         // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_random
         var bombElevation = Math.floor(Math.random() * 400.0) + 400.0;
 
         // https://www.w3schools.com/js/js_objects.asp
-        var someBomb = { elevation: bombElevation};
+        var someOscillator = audioCtx.createOscillator();
+        var someBomb = {
+            elevation: bombElevation,
+            oscillator: audioCtx.createOscillator()
+        };
         bombs.push(someBomb);
     }
 
     // Create the oscillators for the bombs
-    var someOscillator;
     var someGainNode;
     var someBomb;
     for (var j = 0; j < bombs.length; j++) {
         someBomb = bombs[j];
-        someOscillator = audioCtx.createOscillator();
         someGainNode = audioCtx.createGain();
 
-        someOscillator.connect(someGainNode);
+        var curBombOscillator = someBomb.oscillator;
+        curBombOscillator.connect(someGainNode);
         someGainNode.connect(audioCtx.destination);
         someGainNode.gain.value = 0.5;
-        someOscillator.type = 'sine';
-        someOscillator.frequency.value = someBomb.elevation;
-        someOscillator.start();
+        curBombOscillator.type = 'sine';
+        curBombOscillator.frequency.value = someBomb.elevation;
+        curBombOscillator.start();
+    }
+
+    // Run every "frame" which I guess is
+    // every 60th of a second...
+    setInterval(updateBombs, 1000.0 / 60.0);
+};
+
+function updateBombs() {
+    for (var j = 0; j < bombs.length; j++) {
+        curBomb = bombs[j];
+        curBomb.elevation = curBomb.elevation - (80.0 / 60.0);
 
         // https://teropa.info/blog/2016/08/10/frequency-and-pitch.html
-        setTimeout(() => someOscillator.frequency.value = oscillator.frequency.value / 2.0, 1000);
-        setTimeout(() => someOscillator.frequency.value = oscillator.frequency.value / 2.0, 2000);
+        var curBomb = bombs[j];
+        var curBombOscillator = curBomb.oscillator;
+        curBombOscillator.frequency.value = curBomb.elevation;
 
-        setTimeout(
-            function () {
-                someOscillator.stop();
-            },
-            5000
-        );
+        if (curBomb.elevation < 50) {
+            curBomb.oscillator.stop();
+        }
     }
-};
+}
