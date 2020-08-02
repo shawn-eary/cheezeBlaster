84c461afdea474200e7bcf798953c4c245e17a52
Shawn O. Eary <seary@uark.edu>
2020-08-02 14:17:47 -0500

    Add project files.

---
+++ js/synth.js
@@ -0,0 +1,44 @@
+// https://stackoverflow.com/questions/879152/how-do-i-make-javascript-beep
+audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+show();
+
+function show() {
+    frequency = document.getElementById("fIn").value;
+    document.getElementById("fOut").innerHTML = frequency + ' Hz';
+
+    switch (document.getElementById("tIn").value * 1) {
+        case 0: type = 'sine'; break;
+        case 1: type = 'square'; break;
+        case 2: type = 'sawtooth'; break;
+        case 3: type = 'triangle'; break;
+    }
+    document.getElementById("tOut").innerHTML = type;
+
+    volume = document.getElementById("vIn").value / 100;
+    document.getElementById("vOut").innerHTML = volume;
+
+    duration = document.getElementById("dIn").value;
+    document.getElementById("dOut").innerHTML = duration + ' ms';
+}
+
+function beep() {
+    var oscillator = audioCtx.createOscillator();
+    var gainNode = audioCtx.createGain();
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
+    gainNode.gain.value = volume;
+    oscillator.frequency.value = frequency;
+    oscillator.type = type;
+
+    oscillator.start();
+
+    setTimeout(
+        function () {
+            oscillator.stop();
+        },
+        duration
+    );
+};
