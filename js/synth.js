77be0c878f1b3342ad23dd574e155e946b9331e7
Shawn O. Eary <seary@uark.edu>
2020-08-03 20:41:47 -0500

    Got bombs semi-randomly falling in audio but there are bugs.  Many bombs fall at first then only one seems to like to fall at a time as it continues to run.

--- js/synth.js
+++ js/synth.js
@@ -24,6 +24,12 @@ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
 THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+/* Higher sub levels will eventually indicated more
+ * difficult play.  Hard code to level 3 right now.
+ * This should always be less than cNUM_MAX_BOMBS but
+ * greater than or equal to one */
+var playerSubLevel = 3;
+
 // I can't believe I'm actually declaring a
 // global variable (gasp).  Shame on me.
 // More globals are sure to come...
@@ -54,19 +60,15 @@ function initAudio() {
         mainBombGains[i] = someGain;
         someOscillator.start();
     }
-
-    // Run every "frame" which I guess is
-    // every 60th of a second...
-    setInterval(updateBombs, 1000.0 / 60.0);
 }
 
 // https://stackoverflow.com/questions/879152/how-do-i-make-javascript-beep
 // https://stackoverflow.com/questions/14308029/playing-a-chord-with-oscillatornodes-using-the-web-audio-api
-function dropBombs() {
-    // Create 2 two cNUM_MAX_BOMBS number of bombs
-    // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_random
-    var numBombs = Math.floor(Math.random() * 2.0) + (cNUM_MAX_BOMBS - 2.0);
-    // var numBombs = 2;
+function begin() {
+    //// Create 2 two cNUM_MAX_BOMBS number of bombs
+    //// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_random
+    //var numBombs = Math.floor(Math.random() * 2.0) + (cNUM_MAX_BOMBS - 2.0);
+    //// var numBombs = 2;
 
     // BEGIN Hack: This needs to be cleaned up later with a
     // better design
@@ -77,6 +79,8 @@ function dropBombs() {
     // END   Hack: This needs to be cleaned up later with a
     // better design
 
+    // Hack: Only create one bomb at first
+    numBombs = 1;
     for (var i = 0; i < numBombs; i++) {
         // Create a bomb between 400 and 800 ("Martian Feet") - Yeah Whatever..
         // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_random
@@ -85,31 +89,100 @@ function dropBombs() {
         // https://www.w3schools.com/js/js_objects.asp
         var someBomb = {
             elevation: bombElevation,
+            originalElevation: bombElevation,
             oscillatorIndex: i,
             active: true
         };
         bombs.push(someBomb);
     }
 
-    // Activate the bomb oscillators
-    var curGain;
-    var curBomb;
-    var curBombOscillator;
-    for (var j = 0; j < bombs.length; j++) {
-        curBomb = bombs[j];
-
-        // https://middleearmedia.com/web-audio-api-oscillators/
-        curBombOscillator =
-            mainBombOscillators[curBomb.oscillatorIndex];
-        curBombOscillator.type = 'sine';
-        curBombOscillator.frequency.value = curBomb.elevation;
-
-        curGain = mainBombGains[curBomb.oscillatorIndex];
-        curGain.gain.value = 0.1;
-    }
+    // Run every "frame" which I guess is
+    // every 60th of a second...
+    setInterval(updateBombs, 1000.0 / 60.0);
 };
 
+function getNumActiveBombs() {
+    var numActiveBombs = 0;
+    for (var j = 0; j < bombs.length; j++) {
+        if (bombs[j].active) {
+            numActiveBombs++;
+        }
+    }
+    return numActiveBombs;
+}
+
+function getTotalBombLifefactor() {
+    var totalLifeFactor = 0.0;
+    for (var j = 0; j < bombs.length; j++) {
+        var curBomb = bombs[j];
+        if (curBomb.active) {
+            var curLFRatio =
+                curBomb.elevation / curBomb.originalElevation;
+            totalLifeFactor += curLFRatio;
+        }
+    }
+    return totalLifeFactor;
+}
+
 function updateBombs() {
+    // At any frame update, there is a small chance that
+    // another bomb might be created.
+    // a) Never allow more bombs than the curent
+    //    playerSubLevel
+    // b) The probabity of annother bomb being created
+    //    should maybe be greatest when existing bombs
+    //    are "old"
+    // c) To keep from breaking the program, there should
+    //    never be more than cNUM_MAX_BOMBS
+    var numActiveBombs = getNumActiveBombs();
+    var totalBombLifeFactor = getTotalBombLifefactor();
+
+    // Hopefully this number will be from zero to 1...
+    var NTBLF = totalBombLifeFactor / numActiveBombs;
+    // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_random
+    var randNumZeroToOne = Math.random();
+
+    // Remove dead bombs
+    // https://www.w3schools.com/js/js_arrays.asp
+    var newBombs = [];
+    for (var i = 0; i < bombs.length; i++) {
+        var curBomb = bombs[i];
+        if (curBomb.active) {
+            newBombs.push(curBomb);
+        }
+    }
+    // BEGIN Hack: This needs to be cleaned up later with a
+    // better design
+    // "Nuke" old Bombs (Just Kidding)
+    while (bombs.length > 0) {
+        bombs.pop();
+    }
+    // END   Hack: This needs to be cleaned up later with a
+    // better design
+    bombs = newBombs;
+
+    if (randNumZeroToOne > NTBLF) {
+        // See if we can create a new bomb
+        if ((bombs.length <= playerSubLevel) &&
+            (bombs.length <= cNUM_MAX_BOMBS)) {
+
+            // we can create a new bomb
+
+            // Create a bomb between 400 and 800 ("Martian Feet") - Yeah Whatever..
+            // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_random
+            var bombElevation = Math.floor(Math.random() * 400.0) + 400.0;
+
+            // https://www.w3schools.com/js/js_objects.asp
+            var someBomb = {
+                elevation: bombElevation,
+                originalElevation: bombElevation,
+                oscillatorIndex: bombs.length,
+                active: true
+            };
+            bombs.push(someBomb);
+        }
+    }
+
     for (var j = 0; j < bombs.length; j++) {
         curBomb = bombs[j];
 
@@ -127,10 +200,15 @@ function updateBombs() {
             curBombOscillator.frequency.value = curBomb.elevation;
 
             // Turn the volume for the oscillator off when the 
-            // "bomb" gets beelow 50 "Martian Feet"
+            // "bomb" gets below 50 "Martian Feet"
             if (curBomb.elevation < 50) {
                 mainBombGains[curBomb.oscillatorIndex].gain.value = 0.0;
                 curBomb.active = false;
+            } else if (mainBombGains[curBomb.oscillatorIndex].gain.value < 0.1) {
+                // Turn occilators on for active bombs
+                // https://middleearmedia.com/web-audio-api-oscillators/
+                mainBombGains[curBomb.oscillatorIndex].gain.value = 0.1;
+                mainBombGains[curBomb.oscillatorIndex].type = 'sine';
             }
         }
     }
