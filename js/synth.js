825cb06e0f20e85f01a8527f0ac6732fb4b2e0bb
Shawn O. Eary <seary@uark.edu>
2020-08-04 20:01:56 -0500

    Make the code a little easier to read.  Apparently got rid of a bug in the process.

--- js/synth.js
+++ js/synth.js
@@ -41,35 +41,50 @@ var bombs = [];
 
 // https://stackoverflow.com/questions/62768780/how-feasible-is-it-to-use-the-oscillator-connect-and-oscillator-disconnect-m
 const cNUM_MAX_BOMBS = 5
-const cNUM_MAIN_OSCILLATORS = cNUM_MAX_BOMBS;
-var mainBombOscillators = [cNUM_MAIN_OSCILLATORS];
-var mainBombGains = [cNUM_MAIN_OSCILLATORS];
 
-// // https://stackoverflow.com/questions/62768780/how-feasible-is-it-to-use-the-oscillator-connect-and-oscillator-disconnect-m
-// Initialize the oscillators
-function initAudio() {
-    var someOscillator;
-    var someGain;
-    for (var i = 0; i < cNUM_MAIN_OSCILLATORS; i++) {
-        someOscillator = audioCtx.createOscillator();
-        someGain = audioCtx.createGain();
-        someOscillator.connect(someGain);
-        someGain.gain.value = 0.0; /* No Sound at First */
-        someGain.connect(audioCtx.destination);
-        mainBombOscillators[i] = someOscillator;
-        mainBombGains[i] = someGain;
-        someOscillator.start();
-    }
+function makeBomb() {
+    // Create a bomb between 400 and 800 ("Martian Feet") - Yeah Whatever..
+    // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_random
+    var bombElevation = Math.floor(Math.random() * 400.0) + 400.0;
+
+    // https://stackoverflow.com/questions/879152/how-do-i-make-javascript-beep
+    // Set up bomb oscillator and gain
+    var someOscillator = audioCtx.createOscillator();
+    var someGain = audioCtx.createGain();
+    someOscillator.connect(someGain);
+    someGain.connect(audioCtx.destination);
+
+    someGain.gain.value = 0.1; 
+    someOscillator.frequency.value = bombElevation;
+    someOscillator.type = 'sine';
+
+    someOscillator.start();
+
+    // https://www.w3schools.com/js/js_objects.asp
+    var someBomb = {
+        elevation: bombElevation,
+        originalElevation: bombElevation,
+        oscillator: someOscillator, 
+        gain: someGain, 
+        active: true
+    };
+    bombs.push(someBomb);
 }
 
-// https://stackoverflow.com/questions/879152/how-do-i-make-javascript-beep
-// https://stackoverflow.com/questions/14308029/playing-a-chord-with-oscillatornodes-using-the-web-audio-api
-function begin() {
-    //// Create 2 two cNUM_MAX_BOMBS number of bombs
-    //// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_random
-    //var numBombs = Math.floor(Math.random() * 2.0) + (cNUM_MAX_BOMBS - 2.0);
-    //// var numBombs = 2;
-
+function nukeDeadBombs() {
+    // Remove dead bombs
+    // https://www.w3schools.com/js/js_arrays.asp
+    var newBombs = [];
+    for (var i = 0; i < bombs.length; i++) {
+        var curBomb = bombs[i];
+        if (curBomb.active) {
+            newBombs.push(curBomb);
+        } else {
+            // Dead bomb. Deactivate the oscillator
+            curBomb.gain.value = 0.0;
+            curBomb.oscillator.stop();
+        }
+    }
     // BEGIN Hack: This needs to be cleaned up later with a
     // better design
     // "Nuke" old Bombs (Just Kidding)
@@ -78,24 +93,12 @@ function begin() {
     }
     // END   Hack: This needs to be cleaned up later with a
     // better design
+    bombs = newBombs;
+}
 
-    // Hack: Only create one bomb at first
-    numBombs = 1;
-    for (var i = 0; i < numBombs; i++) {
-        // Create a bomb between 400 and 800 ("Martian Feet") - Yeah Whatever..
-        // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_random
-        var bombElevation = Math.floor(Math.random() * 400.0) + 400.0;
-
-        // https://www.w3schools.com/js/js_objects.asp
-        var someBomb = {
-            elevation: bombElevation,
-            originalElevation: bombElevation,
-            oscillatorIndex: i,
-            active: true
-        };
-        bombs.push(someBomb);
-    }
-
+// https://stackoverflow.com/questions/879152/how-do-i-make-javascript-beep
+// https://stackoverflow.com/questions/14308029/playing-a-chord-with-oscillatornodes-using-the-web-audio-api
+function begin() {
     // Run every "frame" which I guess is
     // every 60th of a second...
     setInterval(updateBombs, 1000.0 / 60.0);
@@ -125,6 +128,9 @@ function getTotalBombLifefactor() {
 }
 
 function updateBombs() {
+    // "Garbage collect" dead bombs
+    nukeDeadBombs();
+
     // At any frame update, there is a small chance that
     // another bomb might be created.
     // a) Never allow more bombs than the curent
@@ -138,48 +144,20 @@ function updateBombs() {
     var totalBombLifeFactor = getTotalBombLifefactor();
 
     // Hopefully this number will be from zero to 1...
-    var NTBLF = totalBombLifeFactor / numActiveBombs;
+    var NTBLF = 0.0;
+    if (numActiveBombs > 0) {
+        NTBLF = totalBombLifeFactor / numActiveBombs;
+    }
+    
     // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_random
     var randNumZeroToOne = Math.random();
-
-    // Remove dead bombs
-    // https://www.w3schools.com/js/js_arrays.asp
-    var newBombs = [];
-    for (var i = 0; i < bombs.length; i++) {
-        var curBomb = bombs[i];
-        if (curBomb.active) {
-            newBombs.push(curBomb);
-        }
-    }
-    // BEGIN Hack: This needs to be cleaned up later with a
-    // better design
-    // "Nuke" old Bombs (Just Kidding)
-    while (bombs.length > 0) {
-        bombs.pop();
-    }
-    // END   Hack: This needs to be cleaned up later with a
-    // better design
-    bombs = newBombs;
 
     if (randNumZeroToOne > NTBLF) {
         // See if we can create a new bomb
         if ((bombs.length <= playerSubLevel) &&
             (bombs.length <= cNUM_MAX_BOMBS)) {
 
-            // we can create a new bomb
-
-            // Create a bomb between 400 and 800 ("Martian Feet") - Yeah Whatever..
-            // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_random
-            var bombElevation = Math.floor(Math.random() * 400.0) + 400.0;
-
-            // https://www.w3schools.com/js/js_objects.asp
-            var someBomb = {
-                elevation: bombElevation,
-                originalElevation: bombElevation,
-                oscillatorIndex: bombs.length,
-                active: true
-            };
-            bombs.push(someBomb);
+            makeBomb();
         }
     }
 
@@ -195,21 +173,13 @@ function updateBombs() {
 
             // https://teropa.info/blog/2016/08/10/frequency-and-pitch.html
             var curBomb = bombs[j];
-            var curBombOscillator =
-                mainBombOscillators[curBomb.oscillatorIndex];
-            curBombOscillator.frequency.value = curBomb.elevation;
+            curBomb.oscillator.frequency.value = curBomb.elevation;
 
             // Turn the volume for the oscillator off when the 
             // "bomb" gets below 50 "Martian Feet"
             if (curBomb.elevation < 50) {
-                mainBombGains[curBomb.oscillatorIndex].gain.value = 0.0;
                 curBomb.active = false;
-            } else if (mainBombGains[curBomb.oscillatorIndex].gain.value < 0.1) {
-                // Turn occilators on for active bombs
-                // https://middleearmedia.com/web-audio-api-oscillators/
-                mainBombGains[curBomb.oscillatorIndex].gain.value = 0.1;
-                mainBombGains[curBomb.oscillatorIndex].type = 'sine';
-            }
+            } 
         }
     }
 }
